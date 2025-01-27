d18709e59f1765504ecc254c8be3207d69b6e837
Sunny Gupta <119126667+sunnygupta02@users.noreply.github.com>
2025-01-27 19:59:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+let url="http://universities.hipolabs.com/search?name=";
+
+
+
+
+
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",async ()=>{
+
+
+    let inp=document.querySelector("input");
+let country=inp.value;
+
+
+let clgArray=await getuniversity(country);
+show(clgArray);
+
+
+
+})
+
+
+function show(clgArray){
+
+    let clgList=document.querySelector("ul");
+     clgList.innerText="";
+    for(clg of clgArray){
+        console.log(clg.name);
+        let li=document.createElement("li");
+        li.innerText=clg.name;
+
+
+        clgList.appendChild(li);
+    
+    
+    }
+
+
+
+}
+
+
+async function getuniversity(country){
+    try{
+        let res=await axios.get(url+country);
+       return (res.data);
+    }
+catch(e){
+    return ("error",e);
+}
+}
